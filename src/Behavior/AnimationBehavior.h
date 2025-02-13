fec2d299c35392edc053ef7e790c99f7d7fe0e5d
Mat T <mathew.tomberlin@gmail.com>
2025-02-13 13:24:56 -0800

    Update AnimationBehavior to take AnimationParameters

--- src/Behavior/AnimationBehavior.h
+++ src/Behavior/AnimationBehavior.h
@@ -8,9 +8,23 @@ using namespace std;
 namespace CGEngine {
 	enum AnimationState { Ready, Running, Paused };
 
+	struct AnimationParameters {
+		AnimationParameters(float rate = 15.f, float spd = 1.f, int maxFrm = 7, bool loop = true, bool startRun = true) :frameRate(rate), speed(spd), maxFrame(maxFrm), looping(loop), startRunning(startRun) {
+		
+		}
+
+		float frameRate = 15.0f;
+		float speed = 1.0f;
+		int maxFrame = 7;
+		bool looping = true;
+		bool startRunning = false;
+	};
+
 	class AnimationBehavior : public Behavior {
 	public:
-		AnimationBehavior(Body* owner);
+		AnimationBehavior(Body* owner, AnimationParameters params = AnimationParameters());
+		void setParameters(AnimationParameters params);
+		void resetProcessData();
 	private:
 		static ScriptEvent animBehaviorStartEvt;
 		static ScriptEvent calculateFrameLengthEvt;
