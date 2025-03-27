803f84637a4d2cb2058a7dfdd3f30c77c0d11ffa
Mat T <mathew.tomberlin@gmail.com>
2025-03-27 00:24:45 -0700

    Update example to support Model tests

--- src/TilemapScene.cpp
+++ src/TilemapScene.cpp
@@ -8,6 +8,7 @@
 #include "Core/Animation/Animation.h"
 #include "Core/Animation/Animator.h"
 #include "Core/Light/Light.h"
+#include "Core/Mesh/Model.h"
 
 namespace CGEngine {
 	class TilemapScene : public Behavior {
@@ -60,7 +61,51 @@ namespace CGEngine {
             };
 
             ScriptEvent meshConstruction = [](ScArgs args) {
-                MeshData* cubeModel = getCubeModel(1.f);
+                // 1. Test static mesh model import
+                {
+                    // Import static mesh using existing "Mesh.obj"
+                    Model* staticModel = new Model("Mesh.obj");
+					
+                    // Create instance with default transform
+                    id_t staticMeshId = staticModel->instantiate(Transformation3D({ 0, 0, -2 }));
+
+                    // Add rotation animation to validate transform
+                    Body* staticMeshBody = world->bodies.get(staticMeshId);
+                    staticMeshBody->addUpdateScript(new Script([](ScArgs args) {args.caller->get<Mesh*>()->rotate({ 0, time.getDeltaSec() * 100, 0 });}));
+                }
+
+                // 2. Test animated skeletal mesh import
+                {
+                    // Import skeletal mesh using existing "Caveman_Test.fbx"
+                    Model* skeletalModel = new Model("Caveman_Test.fbx");
+                    // Create instance with adjusted transform
+                    id_t skeletalMeshId = skeletalModel->instantiate( Transformation3D({ -1, 2, -5 }, { 0, 180, 0 }, { 0.01f, 0.01f, 0.01f }));
+                }
+
+                // 3. Test manual model creation
+                {
+                    // Create a simple cube mesh data
+                    MeshData* cubeData = getCubeModel(1.0f);
+                    Model* manualModel = new Model(cubeData, "ManualCube");
+                
+                    // Create material for the cube
+                    SurfaceParameters cubeParams = SurfaceParameters(
+                        SurfaceDomain("brick_tile.png"),
+                        SurfaceDomain(16.0f)
+                    );
+                    id_t cubeMaterialId = world->createMaterial(cubeParams);
+                    Material* cubeMaterial = world->getMaterial(cubeMaterialId);
+                
+                    // Create instance with materials
+                    id_t manualMeshId = manualModel->instantiate(Transformation3D({ 0, 0, -5 }),{ cubeMaterial }
+                    );
+                
+                    // Add simple animation to verify transformation
+                    Body* manualBody = world->bodies.get(manualMeshId);
+                    manualBody->addUpdateScript(new Script([](ScArgs args) { args.caller->get<Mesh*>()->rotate({ time.getDeltaSec() * 45, 0, 0 }); }));
+                }
+
+                //MeshData* cubeModel = getCubeModel(1.f);
                 Vector3f cubeScale = { 1,1,0.0000000008f };
 
                 MeshData* planeModel = getPlaneModel(1.f, 0);
@@ -121,61 +166,67 @@ namespace CGEngine {
                 Material* animMat = world->getMaterial(animMatId);
 
                 //Bodies
-                id_t testMeshId = world->create(new Mesh("Mesh.obj", Transformation3D({ -1,1,-2 })));
-                Body* testMeshBody = world->bodies.get(testMeshId);
-
-                Script* ballScript = new Script([](ScArgs args) {
-                    args.caller->get<Mesh*>()->rotate({ 0, time.getDeltaSec() * 100, 0 });
-                    args.caller->get<Mesh*>()->move({ sin(time.getElapsedSec()) * 0.0015f, cos(time.getElapsedSec()) * 0.0015f, 0 });
-                    });
-                testMeshBody->addUpdateScript(ballScript);
-
-                id_t testMesh2Id = world->create(new Mesh("Caveman_Test.fbx", Transformation3D({ -1,2,-5 }, { 0,180,0 }, { 0.01f,0.01f,0.01f }), true));
-                Body* testMesh2Body = world->bodies.get(testMesh2Id);
-                Mesh* testMesh = testMesh2Body->get<Mesh*>();
-                MeshData* meshUpdate = testMesh->getMeshData();
-                Animation* anim = new Animation("Caveman_Test.fbx", meshUpdate);
-                testMesh->setMeshData(meshUpdate);
-                renderer.updateModelData(testMesh);
-                Animator* animator = new Animator(anim);
-                testMesh->setAnimator(animator);
-                cout << "Anim with " << testMesh->getMeshData()->bones.size() << " bones\n";
-
-                id_t planeMeshId = world->create(new Mesh(tilemapModel, Transformation3D({ -15,10,-12 }, cubeScale), { grassMaterial, lavaMaterial, mudMaterial, maskedMaterial }));
-                id_t planeMeshId2 = world->create(new Mesh(tilemapModel, Transformation3D({ -15,10,-8 }, cubeScale), { grassMaterial, lavaMaterial, mudMaterial, maskedMaterial }));
-                
-                id_t planeMeshId3 = world->create(new Mesh(planeModel, Transformation3D({ 0,0,-7 }), {animMat}));
-                Body* animPlaneBody = world->bodies.get(planeMeshId3);
-                
-                Script* scrollScript = new Script([](ScArgs args) {
-                    Material* animMat = args.script->getInputDataPtr<Material>("material");
-                    sec_t lastFrame = args.script->getOutputData<sec_t>("lastFrame");
-                    if (time.getElapsedSec() - lastFrame > 0.25f) {
-                        args.script->setOutputData("lastFrame", time.getElapsedSec());
-                        Vector2f offset = animMat->getParameter<Vector2f>("diffuseTextureOffset").value();
-                        if (offset.x > 0.75f) {
-                            offset.x = 0;
-                        }
-                        animMat->setParameter("diffuseTextureOffset", Vector2f{ offset.x + 0.125f,0 }, ParamType::V2);
-                        animMat->setParameter("opacityTextureOffset", Vector2f{ offset.x + 0.125f,0 }, ParamType::V2);
-                    }
-                    args.caller->get<Mesh*>()->move({ 0, 0, sin(time.getElapsedSec()) * 0.015f });
-                });
-                scrollScript->setInputData("material", animMat);
-                animPlaneBody->addUpdateScript(scrollScript);
-                animPlaneBody->rotate(degrees(180));
-                
-                id_t meshId1 = world->create(new Mesh(cubeModel, Transformation3D({ 0,5,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId2 = world->create(new Mesh(cubeModel, Transformation3D({ 0,-5,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId4 = world->create(new Mesh(cubeModel, Transformation3D({ 5,0,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId5 = world->create(new Mesh(cubeModel, Transformation3D({ -5,0,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId6 = world->create(new Mesh(cubeModel, Transformation3D({ 10,-10,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId7 = world->create(new Mesh(cubeModel, Transformation3D({ -10,-10,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId8 = world->create(new Mesh(cubeModel, Transformation3D({ 10,10,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId9 = world->create(new Mesh(cubeModel, Transformation3D({ -10,10,-10 }, cubeScale), {brickMaterial}));
-                id_t meshId3 = world->create(new Mesh(cubeModel, Transformation3D({ 0,0,-20 }, cubeScale), { grassMaterial }));
-                Body* planeBody = world->bodies.get(meshId3);
-                planeBody->get<Mesh*>()->scale({ 100.f,100.f,0.0001f });
+                //id_t testMeshId = world->create(new Mesh("Mesh.obj", Transformation3D({ -1,1,-2 })));
+                //Body* testMeshBody = world->bodies.get(testMeshId);
+                //
+                //Script* ballScript = new Script([](ScArgs args) {
+                //    args.caller->get<Mesh*>()->rotate({ 0, time.getDeltaSec() * 100, 0 });
+                //    args.caller->get<Mesh*>()->move({ sin(time.getElapsedSec()) * 0.0015f, cos(time.getElapsedSec()) * 0.0015f, 0 });
+                //    // Debug output
+                //    auto pos = args.caller->get<Mesh*>()->getPosition();
+                //
+                //});
+                //testMeshBody->addUpdateScript(ballScript);
+                //
+                //id_t shapesId = world->create(new Mesh("Shapes.fbx", Transformation3D({ -1,2,-5 }, { 0,0,0 }, { 0.01f,0.01f,0.01f })));
+                //id_t testMesh2Id = world->create(new Mesh("Caveman_Test.fbx", Transformation3D({ -1,2,-5 }, { 0,180,0 }, { 0.01f,0.01f,0.01f })));
+                //Body* testMesh2Body = world->bodies.get(testMesh2Id);
+                //Mesh* testMesh = testMesh2Body->get<Mesh*>();
+                //MeshData* meshUpdate = testMesh->getMeshData();
+                //Animation* anim = new Animation("Caveman_Test.fbx", meshUpdate);
+                //Animator* animator = new Animator(anim);
+                //testMesh->setAnimator(animator);
+                //// Debug output
+                //cout << "Mesh name: " << testMesh->getMeshName() << "\n";
+                //cout << "Bone count: " << testMesh->getMeshData()->bones.size() << "\n";
+                //cout << "Has animator: " << (testMesh->getAnimator() != nullptr) << "\n";
+                //cout << "Anim with " << testMesh->getMeshData()->bones.size() << " bones\n";
+                //
+                //id_t planeMeshId = world->create(new Mesh(tilemapModel, Transformation3D({ -15,10,-12 }, cubeScale), { grassMaterial, lavaMaterial, mudMaterial, maskedMaterial }));
+                //id_t planeMeshId2 = world->create(new Mesh(tilemapModel, Transformation3D({ -15,10,-8 }, cubeScale), { grassMaterial, lavaMaterial, mudMaterial, maskedMaterial }));
+                //
+                //id_t planeMeshId3 = world->create(new Mesh(planeModel, Transformation3D({ 0,0,-7 }), {animMat}));
+                //Body* animPlaneBody = world->bodies.get(planeMeshId3);
+                //
+                //Script* scrollScript = new Script([](ScArgs args) {
+                //    Material* animMat = args.script->getInputDataPtr<Material>("material");
+                //    sec_t lastFrame = args.script->getOutputData<sec_t>("lastFrame");
+                //    if (time.getElapsedSec() - lastFrame > 0.25f) {
+                //        args.script->setOutputData("lastFrame", time.getElapsedSec());
+                //        Vector2f offset = animMat->getParameter<Vector2f>("diffuseTextureOffset").value();
+                //        if (offset.x > 0.75f) {
+                //            offset.x = 0;
+                //        }
+                //        animMat->setParameter("diffuseTextureOffset", Vector2f{ offset.x + 0.125f,0 }, ParamType::V2);
+                //        animMat->setParameter("opacityTextureOffset", Vector2f{ offset.x + 0.125f,0 }, ParamType::V2);
+                //    }
+                //    args.caller->get<Mesh*>()->move({ 0, 0, sin(time.getElapsedSec()) * 0.015f });
+                //});
+                //scrollScript->setInputData("material", animMat);
+                //animPlaneBody->addUpdateScript(scrollScript);
+                //animPlaneBody->rotate(degrees(180));
+                //
+                //id_t meshId1 = world->create(new Mesh(cubeModel, Transformation3D({ 0,5,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId2 = world->create(new Mesh(cubeModel, Transformation3D({ 0,-5,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId4 = world->create(new Mesh(cubeModel, Transformation3D({ 5,0,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId5 = world->create(new Mesh(cubeModel, Transformation3D({ -5,0,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId6 = world->create(new Mesh(cubeModel, Transformation3D({ 10,-10,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId7 = world->create(new Mesh(cubeModel, Transformation3D({ -10,-10,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId8 = world->create(new Mesh(cubeModel, Transformation3D({ 10,10,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId9 = world->create(new Mesh(cubeModel, Transformation3D({ -10,10,-10 }, cubeScale), {brickMaterial}));
+                //id_t meshId3 = world->create(new Mesh(cubeModel, Transformation3D({ 0,0,-20 }, cubeScale), { grassMaterial }));
+                //Body* planeBody = world->bodies.get(meshId3);
+                //planeBody->get<Mesh*>()->scale({ 100.f,100.f,0.0001f });
             };
 
             Behavior* playerScene = new Behavior(nullptr); 
